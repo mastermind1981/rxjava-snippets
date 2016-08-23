e839bf94f92c6a9d7d127ae8a4fa812b8f9d5e95
Victor J Grazi <vgrazi@gmail.com>
2016-08-23 19:11:19 -0400

    attaching one

--- src/main/java/com/vgrazi/observable/Launcher.java
+++ src/main/java/com/vgrazi/observable/Launcher.java
@@ -1,15 +1,6 @@
 package com.vgrazi.observable;
 
-import com.vgrazi.play.PriceTick;
-import com.vgrazi.play.SomeFeed;
-import com.vgrazi.play.SomeListener;
 import rx.Observable;
-import rx.Subscription;
-import rx.functions.Func1;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by vgrazi on 8/23/16.
@@ -21,25 +12,9 @@ public class Launcher {
   }
 
   private void launch() throws InterruptedException {
-    SomeFeed feed = new SomeFeed(3);
-    Observable<PriceTick> observable = Observable.create(s ->
-      feed.register(new SomeListener() {
-        @Override
-        public void priceTick(PriceTick event) {
-          s.onNext(event);
-        }
-
-        @Override
-        public void error(Throwable throwable) {
-          s.onError(throwable);
-        }
-
-      })
-    );
-
-    Observable<PriceTick> take = observable.take(20);
-    take.subscribe(System.out::println);
+    Observable.just("Hello, World")
+      .subscribe(System.out::println);
   }
 }
 
-// 03_attach_one
+// 04_attach_two
